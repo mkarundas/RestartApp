cece35a9de070833d873137968d3c26445c493e5
Arundas MK <arun.das@reflectionsinfos.com>
2022-05-03 11:00:55 +0530

    Project created

---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Arundas MK on 03/05/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
